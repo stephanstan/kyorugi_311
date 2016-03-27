94040d15aa5533070d349f1f604a3d769e8a0fe5
stephanstan <stephanstan@gmail.com>
2016-03-27 15:51:44 -0600

    test exploring listing instances of a Domain

--- src/integration-test/groovy/kyorugi/DojangControllerSpecTests.groovy
+++ src/integration-test/groovy/kyorugi/DojangControllerSpecTests.groovy
@@ -57,4 +57,61 @@ class DojangControllerSpecTests extends GebSpec {
         println(master.name)
     }
 
+    void "save a new Dojang and examine all the Dojangs in the system"() {
+        given: "A brand new user"
+
+        def master = new Dojang(name: 'Stephan Stan')
+
+        when: "The Dojang is saved"
+        master.save()
+
+        then: "new entry is saved and can be found in the database"
+        master.errors.errorCount == 0
+        master.id != null
+       // println(master.name)
+
+       // def List list
+       //  = Dojang.findAll()
+
+        println "count of Dojang list: " + Dojang.findAll().size()
+        assert Dojang.findAll().size() == 3
+
+        println Dojang.findAll().get(0).name
+        println Dojang.findAll().get(1).name
+        println Dojang.findAll().get(2).name
+
+        // order is not guaranteed
+//        assert Dojang.findAll().get(0).name == "Red Phoenix"
+  //      assert Dojang.findAll().get(0).name == "Precision Taekwondo"
+    //    assert Dojang.findAll().get(0).name == "Stephan Stan"
+    }
+
+
+    void "Save a new Dojang and examine all the Dojangs in the system"() {
+        given: "A brand new user"
+
+        def master = new Dojang(name: 'Stephan Stan')
+
+        when: "The Dojang is saved"
+        master.save()
+
+        then: "new entry is saved and can be found in the database"
+        master.errors.errorCount == 0
+        master.id != null
+        println(master.name)
+
+        // def List list
+        //  = Dojang.findAll()
+
+        println "count of Dojang list: " + Dojang.list().size()
+        assert Dojang.list().size() == 3
+
+     //   println Doijang.findAll().get(0).name
+      //  println Dojang.findAll().get(1).name
+      //  println Dojang.findAll().get(2).name
+
+ //       assert Dojang.findAll().get(0).name == "Red Phoenix"
+   //     assert Dojang.findAll().get(0).name == "Precision Taekwondo"
+     //   assert Dojang.findAll().get(0).name == "Stephan Stan"
+    }
 }
